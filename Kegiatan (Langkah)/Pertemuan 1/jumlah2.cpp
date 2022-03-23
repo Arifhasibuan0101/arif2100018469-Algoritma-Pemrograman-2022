74b8b21cd79ebe4a4fa6873d99a204306ffd76e9
Arifhasibuan0101 <ariefsyahbanna@gmail.com>
2022-03-24 03:24:03 +0700

    file praktikum alpro

---
+++ Kegiatan (Langkah)/Pertemuan 1/jumlah2.cpp
@@ -0,0 +1,12 @@
+ #include <iostream>
+ #include <conio.h>
+ #include "_hitung.h"
+ 
+ main() {
+    Hitung X;
+    cin >> X;
+    X.hitung_jumlahnya();
+    cout << X;
+    getch();
+    return 0;
+ }
